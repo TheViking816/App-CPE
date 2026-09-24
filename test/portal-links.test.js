be811088a8eadb44909991e2065745bb6c108fbb
TheViking816 <adrianlujan94@yahoo.es>
2026-09-24 22:14:27 +0200

    Group Noray links by portal consults and requests

--- test/portal-links.test.js
+++ test/portal-links.test.js
@@ -4,11 +4,11 @@ import { PORTAL_HOME_URL, PORTAL_LINK_GROUPS } from "../src/portalLinks.js";
 
 test("aparecen los nueve iframes autónomos verificados sin credenciales fijas", () => {
   const links = PORTAL_LINK_GROUPS.flatMap((group) => group.links);
-  assert.equal(PORTAL_LINK_GROUPS.length, 4);
-  assert.deepEqual(links.map((link) => link.section).sort(), [
-    "chapero", "chapero-especialidades", "disponibilidad12m", "dobles",
-    "donde-voy", "jornada-contratada", "jornales", "mis-especialidades", "puertas"
-  ].sort());
+  assert.deepEqual(PORTAL_LINK_GROUPS.map((group) => group.title), ["Consultas", "Solicitudes"]);
+  assert.deepEqual(links.map((link) => link.section), [
+    "donde-voy", "jornales", "mis-especialidades", "disponibilidad12m", "puertas",
+    "chapero", "chapero-especialidades", "jornada-contratada", "dobles"
+  ]);
   assert.ok(links.every((link) => !link.url && link.label));
 });
 
